857bd3141960661ac29f4e166e9a34dceb2c9313
IshNarayanTripathi <84721654+IshNarayanTripathi@users.noreply.github.com>
2026-05-22 09:30:28 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      mapi = {}
+      for i, num in enumerate(nums):
+        complement = target-num
+        if complement in mapi:
+            return [mapi[complement], i]
+        mapi[num] = i
+      
